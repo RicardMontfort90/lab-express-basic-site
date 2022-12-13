717dac4574c416e05465862fd238465073465e2a
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2022-12-13 17:25:21 +0100

    work in progress

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res, next) => {
-    // __dirname es una variable de entorno
-    res.status(200).sendFile(__dirname + '/views/home.html')
-  })
+app.get("/", (req,res) => {
+  res.status(200).sendFile(__dirname + "/views/home.html");
+});
 
-  app.get('/about', (req, res, next) => {
-    res.sendFile(__dirname + '/views/about.html')
-  })
-  
-  
+app.get("/about", (req,res) => {
+  res.status(200).sendFile(__dirname + "/views/about.html");
+});
 
+app.get("/works", (req,res) => {
+  res.status(200).sendFile(__dirname + "/views/works.html");
+});
 
-app.listen(3000, () => {
-    console.log("hello world")
-})
+app.listen(3000);
